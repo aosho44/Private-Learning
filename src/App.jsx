09ed875c4c15a0f593eecaa7e22419507a475d53
Ayo Osho <aosho44@gmail.com>
2022-08-17 05:26:27 +0100

    Change from js to jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MyButton from './components/Tutorial';
+
+function App() {
+	return (
+		<div>
+			<h1>Tutorial from the React Website</h1>
+			<MyButton />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Tutorial.jsx
@@ -0,0 +1,5 @@
+export default MyButton;
+
+function MyButton() {
+	return <button>Click Me</button>;
+}
